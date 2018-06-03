13a39e1f0870794993e15d4210b865fbe830139b
Tony <tony@olm-mha.local>
2018-06-03 16:55:11 -0400

    refactors

---
+++ graphql_demos/_mutation_socialNetwork/gqlApp.js
@@ -0,0 +1,82 @@
+const { uniq } = require("lodash");
+const makeExecutableSchema = require("graphql-tools").makeExecutableSchema;
+const { users } = require("./data.json");
+
+const resolveUser = (_, { name: queryName }) =>
+  users
+    .filter(({ name }) => name === queryName)
+    .map(({ name, favoriteColor, friends }) => ({
+      name: () => name,
+      favoriteColor: () => favoriteColor,
+      friends: () =>
+        friends.map(friendName => resolveUser(_, { name: friendName }))
+    }))[0];
+
+const newUser = (_, { userData: { name, favoriteColor } }) => {
+  const newUser = {
+    name,
+    favoriteColor,
+    friends: []
+  };
+  users.push(newUser);
+  return newUser;
+};
+
+const newFriendship = (
+  _,
+  { friendship: { from: fromUserName, to: toUserName } }
+) => {
+  const fromUser = users.find(({ name }) => name === fromUserName);
+  const toUser = users.find(({ name }) => name === toUserName);
+  if (fromUser && toUserName) {
+    fromUser.friends = uniq([...fromUser.friends, toUserName]);
+    toUser.friends = uniq([...toUser.friends, fromUserName]);
+  }
+  return {
+    from: () => resolveUser(_, { name: fromUserName }),
+    to: () => resolveUser(_, { name: toUserName })
+  };
+};
+
+module.exports = makeExecutableSchema({
+  typeDefs: `
+    type User {
+      name: String
+      favoriteColor: String
+      friends: [User]
+    }
+
+    type Friendship {
+      from: User!
+      to: User!
+    }
+
+    type Query {
+      user(name:String!): User
+    }
+
+    input UserInput {
+      name: String!
+      favoriteColor: String!
+    }
+
+    input FriendshipInput {
+      from: String!
+      to: String!
+    }
+
+    type Mutation {
+      newUser(userData: UserInput!): User
+      newFriendship(friendship: FriendshipInput): Friendship
+    }
+  `,
+  resolvers: {
+    Query: {
+      user: resolveUser
+    },
+    Mutation: {
+      newUser: newUser,
+      newFriendship: newFriendship
+    }
+  }
+});
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const GQL_APP = process.env.GQL_APP || "demo-socialNetwork";
+const GQL_APP = process.env.GQL_APP || "_mutation_socialNetwork";
 const PORT = process.env.PORT || 3000;
 const { isEqual } = require("lodash");
 const express = require("express");
@@ -8,17 +8,19 @@ const graphiqlExpress = require("graphql-server-express").graphiqlExpress;
 const fetch = require("node-fetch");
 const cors = require("cors");
 
-const schema = require(`./${GQL_APP}/gqlApp`);
-const targetData = require(`./${GQL_APP}/targetData.json`);
+const schema = require(`./graphql_demos/${GQL_APP}/gqlApp`);
+const targetData = require(`./graphql_demos/${GQL_APP}/targetData.json`);
 
 const app = express();
 app.use(cors(), bodyParser.json(), bodyParser.urlencoded({ extended: false }));
-app.use("/graphql_private", graphqlExpress({ schema, context: {} }));
+app.use("/graphql", graphqlExpress({ schema, context: {} }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql_game" }));
 
-app.use("/graphql", (req, res) => {
-  const { query, variables } = req.body;
+// these endpoints are only here for the game during the workshop
+app.use("/graphql_game", (req, res) => {
   const userName = req.headers["user-name"];
-  fetch(`http://localhost:${PORT}/graphql_private`, {
+  const { query, variables } = req.body;
+  fetch(`http://localhost:${PORT}/graphql`, {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify({ query, variables })
@@ -26,13 +28,20 @@ app.use("/graphql", (req, res) => {
     .then(res => res.json())
     .then(data => {
       if (isEqual(targetData, data)) {
+        console.log("=======================");
         console.log("CORRECT!!!!: ", userName);
+        console.log("=======================");
       }
       res.send(data);
     });
 });
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+app.use("/graphiql_game", graphiqlExpress({ endpointURL: "/graphql_game" }));
 
 app.listen(PORT, () => {
-  console.log(`listening on port: ${PORT}`);
+  console.log("===============================================");
+  console.log(`| graphql:  http://localhost:${PORT}/graphql`);
+  console.log(`| graphiql: http://localhost:${PORT}/graphiql`);
+  console.log("-----------------------------------------------");
+  console.log(`| game:     http://localhost:${PORT}/graphiql_game`);
+  console.log("===============================================");
 });
