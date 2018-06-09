9e946692b2e65b928c6068dab8d601280eb60d88
Tony <tony@olm-mha.local>
2018-06-09 01:49:27 -0400

    consolidate to one service

--- index.js
+++ index.js
@@ -8,34 +8,46 @@ const graphiqlExpress = require("apollo-server-express").graphiqlExpress;
 const fetch = require("node-fetch");
 const cors = require("cors");
 
-const schema = require(`./graphql_demos/${GQL_APP}/gqlApp`);
-const targetData = require(`./graphql_demos/${GQL_APP}/targetData.json`);
-
 const app = express();
 app.use(cors(), bodyParser.json(), bodyParser.urlencoded({ extended: false }));
-app.use("/graphql", graphqlExpress({ schema, context: {} }));
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql_game" }));
 
-// these endpoints are only here for the game during the workshop
-app.use("/graphql_game", (req, res) => {
-  const userName = req.headers["user-name"];
-  const { query, variables } = req.body;
-  fetch(`http://localhost:${PORT}/graphql`, {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ query, variables })
-  })
-    .then(res => res.json())
-    .then(data => {
-      if (isEqual(targetData, data)) {
-        console.log("=======================");
-        console.log("CORRECT!!!!: ", userName);
-        console.log("=======================");
-      }
-      res.send(data);
-    });
+[
+  "_mutation_socialNetwork",
+  "inventorySystem",
+  "randomQuotes",
+  "randomQuotes_normalizedData",
+  "socialNetwork"
+].forEach(projectId => {
+  const schema = require(`./graphql_demos/${projectId}/gqlApp`);
+  const targetData = require(`./graphql_demos/${projectId}/targetData.json`);
+  app.use(`/${projectId}/graphql`, graphqlExpress({ schema, context: {} }));
+  app.use(
+    `/${projectId}/graphiql`,
+    graphiqlExpress({ endpointURL: `/${projectId}/graphql_game` })
+  );
+  app.use(`/${projectId}/graphql_game`, (req, res) => {
+    const userName = req.headers["user-name"];
+    const { query, variables } = req.body;
+    fetch(`http://localhost:${PORT}/${projectId}/graphql`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ query, variables })
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (isEqual(targetData, data)) {
+          console.log("=======================");
+          console.log("CORRECT!!!!: ", userName);
+          console.log("=======================");
+        }
+        res.send(data);
+      });
+  });
+  app.use(
+    `/${projectId}/graphiql_game`,
+    graphiqlExpress({ endpointURL: `/${projectId}/graphql_game` })
+  );
 });
-app.use("/graphiql_game", graphiqlExpress({ endpointURL: "/graphql_game" }));
 
 app.listen(PORT, () => {
   console.log("===============================================");
