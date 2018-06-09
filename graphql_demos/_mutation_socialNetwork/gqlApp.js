ad6e93125c4f577af95d626e265c00547532a15e
Tony <tony@olm-mha.local>
2018-06-08 21:09:13 -0400

    updates mutation game

--- graphql_demos/_mutation_socialNetwork/gqlApp.js
+++ graphql_demos/_mutation_socialNetwork/gqlApp.js
@@ -1,6 +1,14 @@
 const { uniq } = require("lodash");
 const makeExecutableSchema = require("graphql-tools").makeExecutableSchema;
 const { users } = require("./data.json");
+const fs = require("fs");
+
+const writeToDisk = () => {
+  fs.writeFileSync(
+    "graphql_demos/_mutation_socialNetwork/data.json",
+    JSON.stringify({ users }, null, 2)
+  );
+};
 
 const resolveUser = (_, { id: queryId }) =>
   users
@@ -9,11 +17,16 @@ const resolveUser = (_, { id: queryId }) =>
       id: id,
       name: () => name,
       favoriteColor: () => favoriteColor,
-      friends: () => friends.map(friendId => resolveUser(_, { id: friendId }))
+      friends: ({ id: queryFriendId }) =>
+        queryFriendId
+          ? friends
+              .filter(friendId => friendId === queryFriendId)
+              .map(friendId => resolveUser(_, { id: friendId }))
+          : friends.map(friendId => resolveUser(_, { id: friendId }))
     }))[0];
 
 const newUser = (_, { userData: { name, favoriteColor } }) => {
-  const id = users.length;
+  const id = users.length + 1;
   const newUser = {
     id,
     name,
@@ -21,6 +34,7 @@ const newUser = (_, { userData: { name, favoriteColor } }) => {
     friends: []
   };
   users.push(newUser);
+  writeToDisk();
   return resolveUser(_, {
     id: newUser.id
   });
@@ -30,12 +44,13 @@ const newFriendship = (
   _,
   { friendship: { from: fromUserId, to: toUserId } }
 ) => {
-  const fromUser = users.find(({ name }) => name === fromUserId);
-  const toUser = users.find(({ name }) => name === toUserId);
-  if (fromUser && toUserId) {
+  const fromUser = users.find(({ id }) => id === fromUserId);
+  const toUser = users.find(({ id }) => id === toUserId);
+  if (fromUser && toUser) {
     fromUser.friends = uniq([...fromUser.friends, toUserId]);
     toUser.friends = uniq([...toUser.friends, fromUserId]);
   }
+  writeToDisk();
   return {
     from: () => resolveUser(_, { id: fromUserId }),
     to: () => resolveUser(_, { id: toUserId })
@@ -45,15 +60,15 @@ const newFriendship = (
 module.exports = makeExecutableSchema({
   typeDefs: `
     type User{
-      id: Int
+      id: Int!
       name: String
       favoriteColor: String
-      friends: [User]
+      friends(id: Int): [User]
     }
 
     type Friendship {
-      from: User!
-      to: User!
+      from: User
+      to: User
     }
 
     type Query {
